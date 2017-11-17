d3f3a692ffd79e672e67edad843710c935af42a5
James Yu <jamesyu717@163.com>
2017-11-17 13:02:49 +0800

    update no.1

--- No.1_say_hello.py
+++ No.1_say_hello.py
@@ -20,5 +20,5 @@ Hello, Brian, nice to meet you!
 name = input("What is your name?")
 print("Hello,",name+",","nice to meet you!")
 
-#如何在“name”和“nice to meet you!”之间添加一个逗号,且逗号紧挨着“name”？
-#【√】直接在后面用“+”连接
+#【问】如何在“name”和“nice to meet you!”之间添加一个逗号,且逗号紧挨着“name”？
+#【答】直接在后面用“+”连接
